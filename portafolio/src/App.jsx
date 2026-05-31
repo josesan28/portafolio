eb28271e3a59d72fde16f293667ba959a280976c
josesan28 <san24092@uvg.edu.gt>
2026-05-30 20:30:34 -0600

    Configuración inicial del proyecto

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Portafolio</div>} />
+    </Routes>
+  )
+}
+
+export default App
